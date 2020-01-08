7c66c36d4c2f84406d6209e6d3bedff0986e3ed6
Vu Kim Duy <duyvukim752000@gmail.com>
2020-01-08 22:14:34 +1100

    Testing Songlist

---
+++ app/src/main/java/com/example/HomeActivity/playSongFunction.java
@@ -0,0 +1,16 @@
+package com.example.HomeActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.LoginPackage.R;
+
+public class playSongFunction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song_function);
+    }
+}
