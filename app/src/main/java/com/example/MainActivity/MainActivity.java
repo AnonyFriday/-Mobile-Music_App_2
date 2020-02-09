6b981a4460a4df725f558478e0e957ed088b5a9c
Vu Kim Duy <duyvukim752000@gmail.com>
2020-02-09 18:17:34 +1100

    Finished Project

--- app/src/main/java/com/example/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/MainActivity/MainActivity.java
@@ -1,82 +1,64 @@
 package com.example.MainActivity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
-import com.example.SettingActivity.RatingbarFragment;
-import com.example.SettingActivity.SettingFragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.LoginPackage.R;
 import com.example.SettingActivity.SettingFragment;
 import com.example.SingerPackage.SingerFragment;
-import com.example.SongPackage.Database.SongDataBaseHelper;
-import com.example.SongPackage.RecyclerView.SongRecyclerViewAdapter;
 import com.example.SongPackage.SongFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    BottomNavigationView mBottomNavigationView;
-    static SingerFragment singerFragment;
-    static SongFragment songFragment;
-    static SettingFragment settingFragment;
-
-    ImageView userImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userImageView = findViewById(R.id.user_ImageView_main);
 
-        songFragment = new SongFragment();
+        SongFragment songFragment = new SongFragment();
         this.getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frameLayoutContainer,songFragment)
+                .replace(R.id.frameLayoutContainer, songFragment)
                 .commit();
 
+        ImageView userImageView = findViewById(R.id.user_ImageView_main);
+
         //Initialize the menu listener
-        mBottomNavigationView = findViewById(R.id.bottomNavigationView);
+        BottomNavigationView mBottomNavigationView = findViewById(R.id.bottomNavigationView);
         mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-                switch (menuItem.getItemId())
-                {
-                    case R.id.navigation_singer:
-                    {
-                        singerFragment = new SingerFragment();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_singer: {
+                        SingerFragment singerFragment = new SingerFragment();
                         getSupportFragmentManager()
                                 .beginTransaction()
-                                .replace(R.id.frameLayoutContainer,singerFragment)
+                                .replace(R.id.frameLayoutContainer, singerFragment)
                                 .commit();
 
                         return true;
                     }
-                    case R.id.navigation_song:
-                    {
-                        songFragment = new SongFragment();
+                    case R.id.navigation_song: {
+                        SongFragment songFragment = new SongFragment();
                         getSupportFragmentManager()
                                 .beginTransaction()
-                                .replace(R.id.frameLayoutContainer,songFragment)
+                                .replace(R.id.frameLayoutContainer, songFragment)
                                 .commit();
 
                         return true;
                     }
-                    case R.id.navigation_setting:
-                    {
-                        settingFragment = new SettingFragment();
+                    case R.id.navigation_setting: {
+                        SettingFragment settingFragment = new SettingFragment();
                         getSupportFragmentManager()
                                 .beginTransaction()
-                                .replace(R.id.frameLayoutContainer,settingFragment)
+                                .replace(R.id.frameLayoutContainer, settingFragment)
                                 .commit();
-
 
 
                         return true;
@@ -86,9 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
-
 
 
     }
